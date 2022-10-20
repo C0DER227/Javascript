27e097530fa66cfae877bbbf7c862fe26da57509
Coder22794 <inbuiltcoder22794@gmail.com>
2022-10-20 22:54:45 +0530

    LuckyNo. Game-(begineer level)

---
+++ luckyNumber/script.js
@@ -0,0 +1,19 @@
+
+const btn = document.querySelector('.btn')
+btn.addEventListener('click', () => {
+    const randnumber = () => {
+        let randnum = Math.floor(Math.random() * 999+1)
+        return randnum
+    }
+    let a = randnumber()
+    if (a > 777) {
+        alert("Number Exceeds as ur greed!!", randnumber(a))
+    } else if (a == 777 && a==999) {
+        console.log("Jackpot Hurray", randnumber(a))
+    } else if (a < 777) {
+        alert('Minimal Range!!!', randnumber(a))
+    } else {
+        alert('Try Again!!!!', randnumber(a))
+    }
+    document.getElementById('container1').innerText=`Number:${a}`
+})
